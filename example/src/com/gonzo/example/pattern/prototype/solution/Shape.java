485ce207eb36f1accf86b3d12b2d07743d0da458
gon-zo <pnw123456@icloud.com>
2022-03-12 19:16:07 +0900

    프로토 타입 예제

---
+++ example/src/com/gonzo/example/pattern/prototype/solution/Shape.java
@@ -0,0 +1,14 @@
+package com.gonzo.example.pattern.prototype.solution;
+
+public class Shape implements Cloneable {
+
+  private String id;
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+}
